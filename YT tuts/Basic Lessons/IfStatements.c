ecece1f49ad5bc70dcb45ea536aad5c6f83244c7
James Espinosa <de.james013@gmail.com>
2022-10-01 20:54:11 +0800

    100122

---
+++ YT tuts/Basic Lessons/IfStatements.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char unit;
+    float temp;
+
+    printf("Enter the unit (C for Celsius, F for Farenheit): ");
+    scanf("%c", &unit);
+
+    unit = toupper(unit);
+    printf("Enter the temperature: ");
+    scanf("%f", &temp);
+    if (unit == 'C')
+    {
+        temp = (temp * 9 / 5) + 32;
+        printf("Farenheit: %0.2f\n", temp);
+    }
+    else if (unit == 'F')
+    {
+        temp = (temp + 32) * 5 / 9;
+        printf("Celsius: %0.2f\n");
+    }
+    else
+    {
+        printf("\nUnit is not recognized.");
+    }
+
+    return 0;
+}
---
+++ YT tuts/Basic Lessons/MathFunctions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double A = sqrt(9);
+    double B = pow(2, 4);
+    int C = round(3.14);
+    int D = ceil(3.14);
+    int E = floor(3.99);
+    double F = log(3);
+    double G = fabs(-69);
+    double H = sin(45);
+    double I = cos(45);
+    double J = tan(45);
+
+    return 0;
+}
