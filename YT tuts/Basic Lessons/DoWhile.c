122a92667fdc63780283770b2476bff8cff8c022
deJames-13 <de.james013@gmail.com>
2022-10-01 21:54:53 +0800

    d

--- YT tuts/Basic Lessons/DoWhile.c
+++ YT tuts/Basic Lessons/DoWhile.c
@@ -5,15 +5,13 @@ int main(int argc, char const *argv[])
 {
     short size = 10;
     char name[size];
-    printf("\nEnter your name? ");
-    fgets(name, size, stdin);
-    name[strlen(name) - 1] = '\0'; // STRING STRIP, REMOVES THE EMPTY SPACE AT THE END OF THE STRING
-    while (strlen(name) == 0)
+
+    do
     {
         printf("\nEnter your name? ");
         fgets(name, size, stdin);
         name[strlen(name) - 1] = '\0'; // STRING STRIP, REMOVES THE EMPTY SPACE AT THE END OF THE STRING
-    }
+    } while (strlen(name) == 0);
     printf("Hello %s", name);
 
     return 0;
---
+++ YT tuts/scratch.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    char str[10];
+    printf("Lol %s", str);
+    return 0;
+}
