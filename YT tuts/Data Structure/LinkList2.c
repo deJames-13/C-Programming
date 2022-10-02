d1b90ee2df074e7512aa189a1a20a09da2e1e871
deJames-13 <de.james013@gmail.com>
2022-10-02 14:35:01 +0800

    desktop

---
+++ YT tuts/Data Structure/LinkList2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+struct Node
+{
+    char data;
+    struct Node *nextNode;
+};
+
+struct LinkedList
+{
+    struct Node *head;
+};
+void showNode(struct Node node)
+{
+    printf("<Node: %c>\n", node.data);
+}
+void showHead(struct LinkedList *l)
+{
+    printf("Head:  ");
+    showNode(*l->head);
+}
+
+int main()
+{
+    struct Node a, b, c;
+    a.data = 'A';
+    c.data = 'C';
+    b.data = 'B';
+    a.nextNode = &b;
+    b.nextNode = &c;
+
+    struct LinkedList mylist;
+    mylist.head = &a;
+
+    showHead(&mylist);
+    showNode(*mylist.head->nextNode);
+    return 0;
+}
