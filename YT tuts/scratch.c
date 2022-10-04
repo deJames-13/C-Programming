b10347fe71a36b7582760649420ebcbbdc967782
James Espinosa <de.james013@gmail.com>
2022-10-04 14:56:17 +0800

    asas

--- YT tuts/scratch.c
+++ YT tuts/scratch.c
@@ -7,6 +7,7 @@ int main(int argc, char const *argv[])
     printf("This is a scratch.\n");
     printf("To compile do gcc <C file dir> -o <Executable file dir/name>\n");
     float ave = 2 / 1.3;
-    printf("%0.2f\n", ave);
+    int div = 12 / 5;
+    printf("%d\n", div);
     return 0;
 }
