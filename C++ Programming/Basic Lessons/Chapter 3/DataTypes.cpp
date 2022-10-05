7070d888591599c9ee99434e7a97395fc9f83f5f
James Espinosa <de.james013@gmail.com>
2022-10-05 21:35:04 +0800

    iadd

---
+++ C++ Programming/Basic Lessons/Chapter 3/DataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Integer
+    int number1 = 13;         // decimal
+    int number2 = 17;         // octal
+    int number3 = 0x0F;       // hexadeci
+    int number4 = 0b00001111; // binary
+
+    cout << "Hello World" << endl;
+    cout << "Number 1: " << number1 << endl;
+    cout << "Number 2: " << number2 << endl;
+    cout << "Number 3: " << number3 << endl;
+    cout << "Number 4: " << number4 << endl;
+    return 0;
+}
--- C++ Programming/scratch.c
+++ C++ Programming/scratch.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 int main()
 {
+    /*
+    char name[] = "Jepoy Dizon";
+    printf("t*ng*n*m*, %s\n", name);
+
+    A string/text is a type of data that contains multiple characters.
+    -String are also known as an array/collection of characters
+    -Strings are enclosed with double quotes
+
+
+    !!! Strings must always have an initial value !!!
+    */
+    char name[] = "";
+
+    printf("Enter your name: ");
+    scanf("%s", &name); // %s is the formatter for strings
+
+    printf("\nHello, %s\n", name);
 
     return 0;
 }
