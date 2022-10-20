7e9f58ae9831b2afe638382cfa122a0bcee863dd
James Espinosa <de.james013@gmail.com>
2022-10-20 11:54:12 +0800

    ass

--- Lessons/Week6/machineproblem2.c
+++ Lessons/Week6/machineproblem2.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
+//#include <windows.h>
 
 void main()
 {
-    char *output;
-    float netSal;
+    int netSal;
     int salary;
-    int cLowSal = 0, aLowSal = 0;
+    int cLowSal = 0, aLowSal = 0; // c for counter, a for accumulator
     int cMidSal = 0, aMidSal = 0;
     int cHighSal = 0, aHighSal = 0;
-    int aveLowSal = 0;
+    int aveLowSal = 0; // ave for average
     int aveMidSal = 0;
     int aveHighSal = 0;
 
 repeat:
+    //	system("cls");
     printf("\nEnter your salary: ");
     scanf("%d", &salary);
-    output = "";
 
     if (salary >= 0 && salary <= 4000)
     {
@@ -25,22 +25,21 @@ repeat:
     }
     else if (salary >= 4001 && salary <= 34999)
     {
-        netSal = salary - (salary * 0.30);
         cMidSal += 1;
         aMidSal = aMidSal + salary;
         aveMidSal = aMidSal / cMidSal;
     }
-    else if (salary > 34999)
+    else
     {
-        netSal = salary - (salary * 0.48);
         cHighSal += 1;
         aHighSal = aHighSal + salary;
         aveHighSal = aHighSal / cLowSal;
     }
-    printf("\nOut for salary that is less than or equal to 4000:\nCounted Salary: %d\nAccumulated Salary: %d\nAverage Salary: %d\nNet Salary Salary: %d\n\n", cLowSal, aLowSal, aveLowSal, netSal);
-    printf("Out for salary that is less than or equal to 34999:\nCounted Salary: %d\nAccumulated Salary: %d\nAverage Salary: %d\nNet Salary Salary: %d\n\n", cMidSal, aMidSal, aveMidSal, netSal);
-    printf("Out for salary that is greater than 34999:\nCounted Salary: %d\nAccumulated Salary: %d\nAverage Salary: %d\nNet Salary Salary: %d\n\n", cHighSal, aHighSal, aveHighSal, netSal);
-
+    netSal = aveLowSal + aveMidSal + aveHighSal;
+    printf("\nOutput for salary that is less than or equal to 4000:\nCounted Salary: %d\nAccumulated Salary: %d\nAverage Salary: %d\n\n", cLowSal, aLowSal, aveLowSal);
+    printf("Output for salary that is less than or equal to 34999:\nCounted Salary: %d\nAccumulated Salary: %d\nAverage Salary: %d\n\n", cMidSal, aMidSal, aveMidSal);
+    printf("Output for salary that is greater than 34999:\nCounted Salary: %d\nAccumulated Salary: %d\nAverage Salary: %d\n\n", cHighSal, aHighSal, aveHighSal);
+    printf("The Net Salary for all input is: %d\n", netSal);
     if (salary > 0)
     {
         goto repeat;
