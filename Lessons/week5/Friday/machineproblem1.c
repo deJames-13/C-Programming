9e57e48435f1a1a8ed063ea82205ac866b153fa4
James Espinosa <de.james013@gmail.com>
2022-10-21 13:44:19 +0800

    as

---
+++ Lessons/week5/Friday/machineproblem1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+    int num;
+    int cEven = 0, cOdd = 0, sum = 0;
+
+repeat:
+    printf("Enter a number: ");
+    scanf("%d", &num);
+}
---
+++ Lessons/week5/Friday/machineproblem2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+void main()
+{
+    char ans;
+    float price, quant = 1.0, netAmt, std, amt_due;
+    float TAX_RATE = 0.05;
+    float acc_std = 0;
+
+repeat:
+    printf("\nEnter the price of the product: ");
+    scanf("%f", &price);
+    scanf("");
+    printf("Enter the quantity of the product: ");
+    scanf("%f", &quant);
+    printf("%f\n", quant);
+
+    netAmt = price * quant;
+    std = netAmt * TAX_RATE;
+    amt_due = netAmt + std;
+    acc_std = acc_std + std;
+
+    printf("The Net Amount Due is: %.2f\n", netAmt);
+    printf("The Sales Tax Due is: %.2f\n", std);
+    printf("The Total Amount Due is: %.2f\n", amt_due);
+    printf("\nAccumulated Total Sales Tax: %.2f\n", acc_std);
+
+ask:
+    printf("\nDo you want to continue? (Yy/Nn) ");
+    ans = tolower(getch());
+    if (ans == 'y')
+    {
+        goto repeat;
+    }
+    else if (ans == 'n')
+    {
+        exit(0);
+    }
+    else
+    {
+        goto ask;
+    }
+    printf("Program Terminated.\n");
+}
