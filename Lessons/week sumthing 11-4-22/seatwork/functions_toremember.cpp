553615bf0de0fe7b4a2562a6f43e70710ecae351
deJames-13 <de.james013@gmail.com>
2022-11-05 11:17:24 +0800

    mimo

---
+++ Lessons/week sumthing 11-4-22/seatwork/functions_toremember.cpp
@@ -0,0 +1,37 @@
+
+// WAG KAKALIMUTAN SA CODE tong first 2 line
+#include <iostream>
+using namespace std;
+
+#include <iomanip>
+// eto para magamit ung setw(), setprecision()
+
+int main()
+{
+    cout << "FUNCTIONS TO REMEMBER" << endl;
+    cout << setw(15);
+    // setw() - set width??
+    // - mag
+
+    return 0;
+}
+
+/*
+common formats ng c/c++ code na pare parehas ang gamit
+
+// basta nagbabalik ng integer na 0
+int main(){
+
+    return 0;
+}
+
+// parehas lang. eto walang ibabalik, parehas empty sila
+void main(){
+
+}
+
+main(){
+
+}
+
+ */
---
+++ Lessons/week sumthing 11-4-22/seatwork/scratch.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int num = 1;
+    cout << "Number: " << num << endl;
+    return 0;
+}
