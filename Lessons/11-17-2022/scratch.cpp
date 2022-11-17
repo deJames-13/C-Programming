1e8faffb94b15dbd0295c89436a44b66a80db979
James Espinosa <de.james013@gmail.com>
2022-11-17 14:48:13 +0800

    aa

---
+++ Lessons/11-17-2022/scratch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int num;
+    // cout << "ARRAYS" << endl;
+    // cin >> num;
+    // printf("ARRAYS");
+
+    // 0 1 2 3 4
+    int arrayName[20] = {1, 2, 3, 4, 5};
+    int val1 = arrayName[0];
+    int val2 = arrayName[1];
+    int val3 = arrayName[2];
+    int val4 = arrayName[3];
+    // cout << "First value: " << val1;
+
+    for (int i = 0; i < 20; i++)
+    {
+        
+        cout << "Value of i: " << i << endl;
+    }
+
+    return 0;
+}
